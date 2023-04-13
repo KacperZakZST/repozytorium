9d7482146d36a1bb101aeb3470062d5b68781182
KacperZakZST <130661419+KacperZakZST@users.noreply.github.com>
2023-04-13 13:28:30 +0200

    Add files via upload

---
+++ Polimorfizm/Polimorfizm/MojeKlasy/Klient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizm.MojeKlasy
+{
+    internal class Klient
+    {
+        public string Nazwa { get; set; }
+        public string NrTelefonu { get; set; }
+        public string Kraj { get; set; }
+        public decimal Kapital { get; set; }
+
+        public Klient(string nazwa, string nrTelefonu, string kraj, decimal kapital)
+        {
+            Nazwa = nazwa;
+            NrTelefonu = nrTelefonu;
+            Kraj = kraj;
+            Kapital = kapital;
+        }
+
+        public override string ToString()
+        {
+            return $"Klient: {Nazwa}, {NrTelefonu}, {Kraj}, kapitał: {Kapital}";
+        }
+
+        public decimal PoliczKapital()
+        {
+            return PoliczKapital(12); 
+        }
+
+        public decimal PoliczKapital(int liczbaMiesiecy)
+        {
+            decimal odsetki = 0.03m;
+            decimal nowyKapital = Kapital;
+
+            for (int i = 0; i< liczbaMiesiecy; i++)
+            {
+                nowyKapital += nowyKapital * odsetki;
+            }
+
+            return nowyKapital;
+        }
+
+
+
+    }
+}
---
+++ Polimorfizm/Polimorfizm/MojeKlasy/KlientOsoba.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizm.MojeKlasy
+{
+    internal class KlientOsoba : Klient
+    {
+        public string imie { get; set; }
+        public string Nazwisko { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+
+        public KlientOsoba(string imie, string nazwisko, DateTime DataUrodzenia, string nrTelefonu,
+        string kraj, decimal Kapital) : base("osoba prawna", nrTelefonu, kraj, Kapital)
+        {
+            this.imie = imie;
+            Nazwisko = nazwisko;
+            DataUrodzenia = DataUrodzenia;
+        }
+
+        public override string ToString()
+        {
+            return $"Imie: {imie}, Nazwisko {Nazwisko}, DataUrodzenia {DataUrodzenia}, {base.ToString()}";
+        }
+
+        public decimal PoliczKapital()
+        {
+            decimal kapital = Kapital;
+            kapital = kapital * 1.02m;
+            return kapital;
+        }
+
+    }
+}
